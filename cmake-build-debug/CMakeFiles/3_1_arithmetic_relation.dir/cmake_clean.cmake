89f180bdecda24897a314718747765aaa653ea8a
zhangcy <2304417358@qq.com>
2023-01-04 10:15:52 +0800

    C语言督学营 - 3.1 算数运算符与关系运算符

---
+++ cmake-build-debug/CMakeFiles/3_1_arithmetic_relation.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3_1_arithmetic_relation.exe"
+  "3_1_arithmetic_relation.exe.manifest"
+  "3_1_arithmetic_relation.pdb"
+  "CMakeFiles/3_1_arithmetic_relation.dir/main.c.obj"
+  "lib3_1_arithmetic_relation.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/3_1_arithmetic_relation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
